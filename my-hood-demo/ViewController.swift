37488e4f60483dc57e51c958679ac0d9e1ee037a
shaynemeyer <shayne.meyer@gmail.com>
2015-12-26 07:59:06 -0800

    Initial Commit

---
+++ my-hood-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-hood-demo
+//
+//  Created by Shayne on 12/26/15.
+//  Copyright © 2015 Maynesoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
